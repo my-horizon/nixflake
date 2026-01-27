09c620af9fa487d349dde3954efa5466c97bdb80
my-horizon <mail@virtualhorizon.de>
2026-01-27 21:55:37 +0100

    switched to latest stable version (25.11)

--- flake.nix
+++ flake.nix
@@ -2,23 +2,23 @@
   description = "my flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     flake-parts.url = "github:hercules-ci/flake-parts";
     nvf.url = "github:notashelf/nvf";
     nvf.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs @ {
-    nixpkgs,
-    flake-parts,
-    nvf,
-    ...
-  }:
-    flake-parts.lib.mkFlake {inherit inputs;}
-    {
+  outputs =
+    inputs@{
+      nixpkgs,
+      flake-parts,
+      nvf,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         ./nixos.nix
       ];
-      systems = ["x86_64-linux"];
+      systems = [ "x86_64-linux" ];
     };
 }
