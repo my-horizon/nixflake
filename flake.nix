e39d472c206825f486ac65eff8863d3ac80bacef
my-horizon <mail@virtualhorizon.de>
2026-02-02 20:58:20 +0100

    systems:x86_64-linux still necessary

--- flake.nix
+++ flake.nix
@@ -9,5 +9,13 @@
     nvf.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      imports = [ (inputs.import-tree ./modules) ];
+    };
 }
