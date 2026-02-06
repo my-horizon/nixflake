d20ffba0569fc347cd81d6be5915614ffd2814e9
my-horizon <mail@virtualhorizon.de>
2026-02-06 19:45:37 +0100

    test of tb module

--- modules/thunderbird.nix
+++ modules/thunderbird.nix
@@ -9,6 +9,29 @@
           isDefault = true;
         };
       };
+      accounts.email.accounts.strato = {
+        primary = true;
+        realName = "";
+        address = "mail@virtualhorizon.de";
+        userName = "mail@virtualhorizon.de";
+
+        imap = {
+          host = "imap.strato.de";
+          port = 993;
+          tls.enable = true;
+        };
+
+        smtp = {
+          host = "smtp.strato.de";
+          port = 465;
+          tls.enable = true;
+        };
+
+        thunderbird = {
+          enable = true;
+          profiles = [ "default" ];
+        };
+      };
     };
   };
 }
